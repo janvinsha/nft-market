e67bb8d2781893f51bda0a764bf78a9a8d266221
Janvinsha <janvinsha@gmail.com>
2021-11-03 09:47:02 +0100

    xyz

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,13 +9,13 @@ module.exports = {
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/e4933973f88c4e18b9f032cbdaf20776`,
       accounts: [
-        "1447537e15027f9570059027e698651bb0d8df6062f9c6818aa95f7db72ee0eb",
+       
       ],
     },
     mainnet: {
       url: `https://polygon-mumbai.infura.io/v3/e4933973f88c4e18b9f032cbdaf20776`,
       accounts: [
-        "1447537e15027f9570059027e698651bb0d8df6062f9c6818aa95f7db72ee0eb",
+    
       ],
     },
   },
